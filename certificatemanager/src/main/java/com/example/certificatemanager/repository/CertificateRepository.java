d652c8ea45c0fe6246a8418c3d5aafcaca65c24b
Daniel M <86433525+danmas221@users.noreply.github.com>
2025-03-24 13:53:48 +0100

    Delete certificatemanager directory

--- certificatemanager/src/main/java/com/example/certificatemanager/repository/CertificateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.certificatemanager.repository;
-
-
-import com.example.certificatemanager.entities.Certificate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CertificateRepository extends JpaRepository<Certificate, Long> {
-    //Hier können benutzerdifinierte Abfragen ergänzt werden
-}
