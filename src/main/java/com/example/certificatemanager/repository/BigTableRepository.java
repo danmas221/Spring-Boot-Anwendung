6b1b7a3c5e8362d90559d2759a552827d966ce28
Daniel M <86433525+danmas221@users.noreply.github.com>
2025-06-03 13:33:13 +0200

    Delete src directory

--- src/main/java/com/example/certificatemanager/repository/BigTableRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.certificatemanager.repository;
-
-import com.example.certificatemanager.entities.BigTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BigTableRepository extends JpaRepository<BigTable, Long> {
-    // Hier können benutzerdifinierte Abfragen ergänzt werden
-}
